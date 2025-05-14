abc2002faf4d733f4a0098863fe4c1c9fb7f18f7
bl_aire <ogoh_blessing@yahoo.com>
2025-05-14 15:11:37 +0100

    Str and int intro

--- str.py
+++ str.py
@@ -1,6 +1,11 @@
-# Get input from user and greet user
-name = input("What is your name? ")
+# Get input from user
+# Strings can be formated using built-in methods. Strip() for instance removes whitespaces from the left and right
+# There is also Capitalize(), Title() etc
+name = input("What is your name? ").strip().title()
 
-# Strings can be formated using built-in methods. Strip() for instance removes whitespaces. There is also Capitalize(), Title() etc
-name = name.strip()
-print(f"Hello {name}")
+# Split user's name into first and last name
+
+first, last = name.split(" ")
+
+# Say hello to user
+print(f"Hello {first}")
