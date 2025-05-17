a7b5174625e76d695a07c62d7d8925d35896dfc0
bl_aire <ogoh_blessing@yahoo.com>
2025-05-17 21:48:42 +0100

    Complete conditionals

---
+++ conditionals/house.py
@@ -0,0 +1,18 @@
+name = input("What is your name? ")
+
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who are you?")
+
+
+# MATCH..Is this a Switch statement?
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who are you?")
