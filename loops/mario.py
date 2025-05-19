108bf949d2372bb9bf8f66822472198f91fb797c
bl_aire <ogoh_blessing@yahoo.com>
2025-05-19 13:20:12 +0100

    Complete loops

--- loops/mario.py
+++ loops/mario.py
@@ -1,8 +1,25 @@
 def main():
-    print_square(5)
+    print_square(3)
+    print_square2(4)
 
 def print_square(n):
+    # For each row in square
     for i in range(n):
-        print("#####")
+
+        # For each brick in row
+        for j in range(n):
+
+            # Print brick
+            print("#", end="")
+
+        # For new line at the end of a row
+        print()
+
+def print_square2(n):
+    for i in range(n):
+        print_row(n)
+
+def print_row(width):
+    print("#" * width)
 
 main()
