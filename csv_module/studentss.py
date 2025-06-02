da43cb72ae5aa0e40f9f105ce6b996c7329e8bbd
bl_aire <ogoh_blessing@yahoo.com>
2025-06-02 21:47:01 +0100

    Csv reader() and DictReader()

--- csv_module/studentss.py
+++ csv_module/studentss.py
@@ -1,15 +1,17 @@
-# Import library for reading csv
+# Import library for reading csv and add column titles in the csv file
 import csv
 
 # Empty array that will house data collected from csv
 students = []
 
 # Open file via file i/o method, read file via csv
-# Loop through csv data rows, deconstruct data in rows and append them in array created earlier
+# Loop through csv data rows and append them in array created earlier
+# csv.reader returns array so we can use row[i] to access data
+# csv.DictReader returns a dictionary
 with open('students.csv') as file:
-    reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "home": home})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
 
 # Loop through students array, sort array via student name key and print output
 # Lambda is an anonymous function that accepts student as a parameter and returns the name variable
