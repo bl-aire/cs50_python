23d8c7380dc240574310f9882595e9304fc150a7
bl_aire <ogoh_blessing@yahoo.com>
2025-06-05 23:19:17 +0100

    Validate user input via regex

--- regex/validate_re.py
+++ regex/validate_re.py
@@ -4,7 +4,11 @@ email = input("Enter your email: ").strip()
 
 # .+ || ..*
 # r"" needed here because we are using \ to escape . in .edu
-if re.search(r".+@.+\.edu", email):
+# r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$"
+# lower() can be used to fix case sensitivity of email
+# ? means zero or one repetition so the parenthesis is optional
+
+if re.search(r"^\w+@(\w+\.)?\w+\.(edu|com|gov)$", email, re.IGNORECASE):
     print("Valid email")
 else:
     print("Invalid email")
