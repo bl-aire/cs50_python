f1909f8e6f296d1e844622b84794ee5c80d59c0e
bl_aire <ogoh_blessing@yahoo.com>
2025-06-12 22:10:45 +0100

    Attempt using dictionary syntax instead

--- OOP/student.py
+++ OOP/student.py
@@ -2,10 +2,20 @@ def main():
     student = get_student()
     print(f"Hello, {student[0]} from {student[1]}!")
 
+    student2 = get_student2()
+    print(f"Hello, {student2['name']} from {student2['house']}!")
+
 def get_student():
     name = input("What is your name? ")
     house = input("What is your name? ")
     return name, house
+
+def get_student2():
+    student = {
+        'name': input("What is your name? "),
+        'house': input("What is your name? ")
+    }
+    return student
 
 if __name__ == "__main__":
     main()
