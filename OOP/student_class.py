74ae29ee735213f448347e8744bf5e04f8c8b7fa
bl_aire <ogoh_blessing@yahoo.com>
2025-06-13 20:25:55 +0100

    Create object using class

---
+++ OOP/student_class.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self):
+        self.house = None
+        self.name = None
+
+def main():
+    student = get_student()
+    print(f"Hello, {student.name} from {student.house}!")
+
+def get_student():
+    # Creating an object using the mold(class created earlier)
+    student = Student()
+    student.name = input("What is your name? ")
+    student.house = input("What is your house? ")
+    return student
+
+if __name__ == "__main__":
+    main()
